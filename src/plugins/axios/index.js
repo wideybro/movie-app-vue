01e0878ca6643def2be08907db182ad0ccf9c971
wideybro <artem.a.babenko@gmail.com>
2020-11-09 19:24:28 +0300

    add interceptors, add array ids, add movies to the Store.

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,11 +1,10 @@
 import axios from "axios";
+import interceptors from "./interceptors";
 
 const instance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
-  params: {
-    apiKey: process.env.VUE_APP_API_KEY,
-    plot: "full",
-  },
 });
+
+interceptors(instance);
 
 export default instance;
--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,16 +1,50 @@
+import IDs from "@/store/mock/imdb_top250.js";
 import axios from "@/plugins/axios";
+import mutations from "@/store/mutations";
+
+function serilizeResponse(movies) {
+  return movies.reduce((acc, movie) => {
+    acc[movie.imdbID] = movie;
+
+    return acc;
+  }, {});
+}
+
+const { MOVIES } = mutations;
 
 const moviesStore = {
   namespaced: true,
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    top250IDs: IDs,
+    moviesPerPage: 12,
+    currentPage: 1,
+    movies: {},
+  },
+  getters: {
+    slicedIDs: ({ top250IDs }) => (from, to) => top250IDs.slice(from, to),
+    currentPage: ({ currentPage }) => currentPage,
+    moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
+  },
+  mutations: {
+    [MOVIES](state, value) {
+      state.movies = value;
+    },
+  },
   actions: {
-    async fetchMovies(context) {
-      console.log(context);
-      // tt0111161
-      const response = await axios.get("/?i=tt0111161");
-      console.log(response);
+    async fetchMovies({ getters, commit }) {
+      try {
+        const { currentPage, moviesPerPage, slicedIDs } = getters;
+        const from = currentPage * moviesPerPage - moviesPerPage;
+        const to = currentPage * moviesPerPage;
+        const moviesToFetch = slicedIDs(from, to);
+
+        const requests = moviesToFetch.map((id) => axios.get(`/?i=${id}`));
+        const response = await Promise.all(requests);
+        const movies = serilizeResponse(response);
+        commit(MOVIES, movies);
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
 };
